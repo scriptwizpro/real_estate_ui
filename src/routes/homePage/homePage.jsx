cd3f8d5e7e98329bda4a80912cdb262507970efd
Dev <androidsoloapps@gmail.com>
2025-01-14 23:59:02 -0500

    feat: Added image container and text wrapper for improved content presentation.

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -3,9 +3,15 @@ import "./homePage.scss";
 function HomePage() {
   return (
     <div className="homePage">
-      <div className="textContainer">text</div>
+      <div className="textContainer"></div>
+      <div className="wrapper">
+        <h1>
+          Find Real Estate & Get Your Dream Place Find Real Estate & Get Your
+          Dream Place
+        </h1>
+      </div>
       <div className="imgContainer">
-      <img src="/bg.png" alt="" />
+        <img src="/bg.png" alt="" />
       </div>
     </div>
   );
