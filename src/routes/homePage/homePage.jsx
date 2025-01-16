efb3bc04b653bd2c80aeb2d1d1acdb427dbae20f
Dev <androidsoloapps@gmail.com>
2025-01-15 22:32:18 -0500

    feat(homePage): add homepage layout and styling
    - Implemented the `HomePage` component with a dynamic real estate theme.
    - Included a `SearchBar` for user interaction.
    - Designed the layout with statistics boxes and a hero section.
    - Styled the page using SCSS for modularity and flexibility.

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,14 +1,35 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 function HomePage() {
   return (
     <div className="homePage">
-      <div className="textContainer"></div>
-      <div className="wrapper">
-        <h1>
-          Find Real Estate & Get Your Dream Place Find Real Estate & Get Your
-          Dream Place
-        </h1>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Welcome to R. E. WebSite, where dreams of finding the perfect property
+            come to life. Whether you’re searching for your dream home, a
+            lucrative investment, or a cozy rental, our platform offers a
+            seamless and personalized experience to meet all your real estate
+            needs.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="imgContainer">
         <img src="/bg.png" alt="" />
