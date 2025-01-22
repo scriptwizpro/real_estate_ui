1386fa10068a212f6966f9b853f9007ecce28861
Dev <androidsoloapps@gmail.com>
2025-01-21 23:44:43 -0500

    feat: add routing structure and ListPage component
    - Set up `react-router-dom` with `createBrowserRouter` for navigation.
    - Added `ListPage` component and basic styling in `listPage.scss`.
    - Configured routes for HomePage and ListPage in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import Navbar from "./components/navbar/Navbar"
 import "./layout.scss"
 import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <HomePage/>
+      ),
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
