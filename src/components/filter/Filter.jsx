021958cdf849f597b7cbaad55cf18ce3c176becf
Dev <androidsoloapps@gmail.com>
2025-01-26 23:40:18 -0500

    feat: add Card component with React Router integration and SCSS styling

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,7 +2,16 @@ import './filter.scss'
 
 function Filter() {
     return (
-        <div className='filter'>Filter</div>
+        <div className='filter'>
+            <h1>Search result for <b>London</b></h1>
+            <div className='top'>
+                <div className="item">
+                    <label htmlFor='city'>Location</label>
+                    <input type='text' id='city' placeholder='City location' />
+                </div>
+            </div>
+            <div className='bottom'></div>
+        </div>
     )
 }
 
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,6 +1,7 @@
 import { listData } from "../../lib/dummydata";
 import './listPage.scss';
 import Filter from "../../components/filter/Filter"
+import Card from "../../components/card/Card"
 
 function ListPage() {    
     const data = listData;
@@ -9,10 +10,13 @@ function ListPage() {
             <div className="listContainer">
             <div className="wrapper">
                 <Filter />
+                {data.map(item => (
+                    <Card key={item.id} item={item} />
+                ))}
             </div>
             </div>   
             <div className="mapContainer">Map</div>           
         </div>;    
 }
 
-export default ListPage
+export default ListPage;
