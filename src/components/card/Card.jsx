895fe700078932684350c10e53208a8d514bca2b
Dev <androidsoloapps@gmail.com>
2025-01-29 22:43:38 -0500

    feat: add Card component with styles
    - Implemented `Card` component using React and React Router
    - Added `card.scss` for styling
    - Displays property details including title, address, price, bedrooms, and bathrooms
    - Includes save and chat icons for user interaction

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,36 @@ function Card({ item }) {
       <Link to={`/${item.id}`} className="imageContainer">
         <img src={item.img} alt="" />
       </Link>
-      <div className="textContainer"></div>
+      <div className="textContainer">
+        <h2 className="title">
+          <link to={`/${item.id}`}>{item.title}</link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
