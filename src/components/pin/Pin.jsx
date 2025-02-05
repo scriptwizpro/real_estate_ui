8013a601f451e264745ee75d4fdedd0e0d6e360c
Dev <androidsoloapps@gmail.com>
2025-02-04 22:57:46 -0500

    feat: add custom map pin with popup details
    - Implemented a custom marker icon for the map using Leaflet.
    - Added a popup to display item details including an image, title, bedroom count, and price.
    - Styled the popup content with a flexible layout using SCSS.

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,10 +1,25 @@
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import L from "leaflet";
+
+// Import the default marker images
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Create a custom marker icon
+const customIcon = new L.Icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
       <Popup>
         <div className="popupContainer">
           <img src={item.img} alt="" />
