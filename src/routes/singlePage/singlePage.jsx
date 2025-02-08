796669b42fa7852df842e175ce560436f7f472f9
Dev <androidsoloapps@gmail.com>
2025-02-07 23:54:56 -0500

    feat: add Slider component with basic styling

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,19 +1,20 @@
 import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
-import { singlePostData } from "../../lib/dummydata";
+// import { singlePostData } from "../../lib/dummydata";
 
 function SinglePage() {
   return (
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider images={singlePostData.images} />
+          {/* <Slider images={singlePostData.images} /> */}
+          <Slider />
           <div className="info">
             <div className="top">
-              <div className="post">
+              {/* <div className="post">
                 <h1>{singlePostData.title}</h1>
                 <p>{singlePostData.description}</p>
-              </div>
+              </div> */}
               <div className="user"></div>
             </div>
             <div className="bottom"></div>
