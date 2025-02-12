fa994ab4e71ee6fc1f257c941d1106bbd87e40ec
Dev <androidsoloapps@gmail.com>
2025-02-11 22:34:55 -0500

    feat: add image slider component with full-screen and thumbnail preview

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -3,6 +3,17 @@ import './slider.scss'
 function Slider({images}){
   return (
     <div className='slider'>
+      <div className="fullSlider">
+        <div className="arrow">
+          <img src="/arrow.png" alt="" />
+        </div>
+        <div className="imgContainer">
+          <img src={images[0]} alt="" />
+        </div>
+        <div className="arrow">
+        <img src="/arrow.png" alt="" />
+        </div>
+      </div>
       <div className="bigImage">
         <img src={images[0]} alt='' />
       </div>
