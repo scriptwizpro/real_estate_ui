cab6f41038421745e00396e4e0b9ae7f8c05d59d
Dev <androidsoloapps@gmail.com>
2025-02-12 23:51:44 -0500

    fix: add missing wrapper for Slider component

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './slider.scss'
 
 function Slider({images}){
+  const [imageIndex, setImageIndex] = useState(null)
   return (
     <div className='slider'>
-      <div className="fullSlider">
+      {imageIndex !==null && (<div className="fullSlider">
         <div className="arrow">
           <img src="/arrow.png" alt="" />
         </div>
@@ -11,11 +13,13 @@ function Slider({images}){
           <img src={images[0]} alt="" />
         </div>
         <div className="arrow">
-        <img src="/arrow.png" alt="" />
+        <img src="/arrow.png" className='right' alt="" />
         </div>
+        <div className="close">X</div>
       </div>
+    )}
       <div className="bigImage">
-        <img src={images[0]} alt='' />
+        <img src={images[0]} alt='' onClick={()=>setImageIndex(0)}/>
       </div>
       <div className="smallImages">
         {images.slice(1).map((image,index)=>(
