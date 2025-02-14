862fdda562b6a72f78deae09b30f761f5a32cd7f
Dev <androidsoloapps@gmail.com>
2025-02-13 23:42:45 -0500

    feat: add image gallery with clickable thumbnails and close functionality

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react';
-import './slider.scss'
+import { useState } from "react";
+import "./slider.scss";
 
-function Slider({images}){
-  const [imageIndex, setImageIndex] = useState(null)
+function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
   return (
-    <div className='slider'>
-      {imageIndex !==null && (<div className="fullSlider">
-        <div className="arrow">
-          <img src="/arrow.png" alt="" />
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img src="/arrow.png" alt="" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow">
+            <img src="/arrow.png" className="right" alt="" />
+          </div>
+          <div className="close" onClick={()=>setImageIndex(null)}>
+            X
+            </div>
         </div>
-        <div className="imgContainer">
-          <img src={images[0]} alt="" />
-        </div>
-        <div className="arrow">
-        <img src="/arrow.png" className='right' alt="" />
-        </div>
-        <div className="close">X</div>
-      </div>
-    )}
+      )}
       <div className="bigImage">
-        <img src={images[0]} alt='' onClick={()=>setImageIndex(0)}/>
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImages">
-        {images.slice(1).map((image,index)=>(
-          <img src={image} alt='' key={index} />
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            alt=""
+            key={index}
+            onClick={() => setImageIndex(index+1)}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
