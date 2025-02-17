3bda6ad60c42051ee88c0e182159b8a02aa21336
Dev <androidsoloapps@gmail.com>
2025-02-16 22:47:01 -0500

    feat: add feature list, sizes, nearby places, and locations wrapper

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,5 +1,6 @@
 import "./singlePage.scss"
 import Slider from "../../components/slider/Slider"
+import Map from "../../components/map/Map"
 import { singlePostData } from "../../lib/dummydata"
 import { userData } from "../../lib/dummydata"
 
@@ -34,9 +35,80 @@ function SinglePage() {
       <div className="features">
         <div className="wrapper">
           <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
           <p className="title">Sizes</p>
+          <div className="sizes">
+            <img src="/size.png" alt="" />
+            <span>80 sqft</span>
+          </div>
+          <div className="sizes">
+            <img src="/bed.png" alt="" />
+            <span>2 beds</span>
+          </div>
+          <div className="sizes">
+            <img src="/bath.png" alt="" />
+            <span>1 bathroom</span>
+          </div>
           <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+          <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
           <p className="title">Locations</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              Save the Place
+            </button>
+          </div>
         </div>
       </div>
     </div>
