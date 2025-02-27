077e7ed33d758554a8da07acfbcbd236b89a3107
Dev <androidsoloapps@gmail.com>
2025-02-26 23:28:26 -0500

    feat: add wrapper component for profile and chat sections
    - Added a wrapper element around user information, list, and saved list sections.
    - Introduced a wrapper for the chat container to structure layout consistently.
    - Improved component hierarchy for better readability and maintenance.

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -2,7 +2,29 @@ import './profilePage.scss'
 
 function ProfilePage(){
   return (
-    <div className='profilePage'>ProfilePage</div>
+    <div className='profilePage'>
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>Avatar: <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /></span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
+    </div>
   )
 }
 
