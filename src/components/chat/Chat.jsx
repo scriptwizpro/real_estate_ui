04ce5332996fb80485a7e7ba7d20d6eb0113fc1e
Dev <androidsoloapps@gmail.com>
2025-03-08 22:09:48 -0500

    feat(wrapper): add chat message wrapper with close button and timestamp

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -48,7 +48,50 @@ function Chat(){
           </p>
         </div>
       </div>
-      <div className="chatBox">box</div>
+      <div className="chatBox">
+        <div className="top">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+            John Doe
+          </div>
+          <span className="close">X</span>
+        </div>
+        <div className="center">
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
     </div>
   );
 }
