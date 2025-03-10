7d5ff7790338bbd69955f2541219c3f6ba6d13c6
Dev <androidsoloapps@gmail.com>
2025-03-09 22:16:02 -0500

    feat: Implement chat UI with message list and chat box layout
    - Added `messages` section with individual message cards, including user avatars and names.
    - Styled message cards with flex layout, padding, and border radius.
    - Created `chatBox` component with top, center, and bottom sections.
    - Styled the chat header with background color, padding, and user info display.
    - Added close button with pointer cursor.
    - Basic message rendering with placeholder content.

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,7 @@
 import './chat.scss'
 
 function Chat(){
+  const [chat, setChat] = useState(null)
   return (
     <div className='chat'>
       <div className="messages">
@@ -61,7 +62,7 @@ function Chat(){
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
-          <div className="chatMessage">
+          <div className="chatMessage own">
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
@@ -69,7 +70,7 @@ function Chat(){
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
-          <div className="chatMessage">
+          <div className="chatMessage own">
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
@@ -77,7 +78,7 @@ function Chat(){
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
-          <div className="chatMessage">
+          <div className="chatMessage own">
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
@@ -85,12 +86,31 @@ function Chat(){
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
           <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
             <p>Lorem ipsum dolor sit amet...</p>
             <span>1 hour ago</span>
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <textarea></textarea>
+          <button>Send</button>
+        </div>
       </div>
     </div>
   );
